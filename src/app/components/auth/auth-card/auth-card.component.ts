c204c256fd9693519aa71876ad0e2a9afaa96582
Zachary Turner <zakkturner@yahoo.com>
2023-05-24 17:31:03 -0400

    form ui animation finished. starting api

--- src/app/components/auth/auth-card/auth-card.component.ts
+++ src/app/components/auth/auth-card/auth-card.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
+import gsap, { Power2, TweenMax } from 'gsap';
 
 @Component({
   selector: 'app-auth-card',
   templateUrl: './auth-card.component.html',
-  styleUrls: ['./auth-card.component.css']
+  styleUrls: ['./auth-card.component.css'],
 })
 export class AuthCardComponent implements OnInit {
+  constructor(private elementRef: ElementRef) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  onInputFocus(labelElement: Element): void {
+    gsap.to(labelElement, 0.3, { y: -35, ease: Power2.easeOut });
   }
-
+  onInputFocusOut(labelElement: Element): void {
+    gsap.to(labelElement, 0.3, { y: 0, ease: Power2.easeOut });
+  }
+  ngOnInit(): void {}
 }
