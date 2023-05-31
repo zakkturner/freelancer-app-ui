81d8c36cae205c4f0d32c1e2f32edc13fd5687b5
Zachary Turner <zakkturner@yahoo.com>
2023-05-31 15:08:59 -0400

    moving from 14 to 15

--- src/app/helpers/validateForm.ts
+++ src/app/helpers/validateForm.ts
@@ -1,13 +1,13 @@
-import { FormControl, FormGroup } from '@angular/forms';
+import { UntypedFormControl, UntypedFormGroup } from '@angular/forms';
 
 export default class ValidateForm {
   // Loop through object and if invalid throw error for the field
-  static validateAllFormFields(formGroup: FormGroup) {
+  static validateAllFormFields(formGroup: UntypedFormGroup) {
     Object.keys(formGroup.controls).forEach((field) => {
       const control = formGroup.get(field);
-      if (control instanceof FormControl) {
+      if (control instanceof UntypedFormControl) {
         control.markAsDirty({ onlySelf: true });
-      } else if (control instanceof FormGroup) {
+      } else if (control instanceof UntypedFormGroup) {
         this.validateAllFormFields(control);
       }
     });
